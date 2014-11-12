64bf41eea849489496855099abfbc30a2e12a495
Saurabh <sbansod@iitk.ac.in>
2014-11-12 12:13:19 +0530

    moved all sensitive info application.yml and edited application.yml.example for readers reference

--- app/mailers/example_mailer.rb
+++ app/mailers/example_mailer.rb
@@ -1,21 +1,27 @@
 class ExampleMailer < ActionMailer::Base
-  default from: "fun4saurabh@gmail.com"
+  default from: "from@example.com"
 
   def sample_email(user)
 
     @user = user
-    mail(to: @user.email, subject: 'Sample Email')	
-    # First, instantiate the Mailgun Client with your API key
 
-   #  mg_client = Mailgun::Client.new ENV['api_key']
+    # Uncomment the following when using SMTP for sending out mails
 
-  	# # Define your message parameters
+    #mail(to: @user.email, subject: 'Sample Email')	
+
+    # End 
+    
+    # Uncomment the following when using mailgun-ruby
+
+    # mg_client = Mailgun::Client.new ENV['api_key']
+
   	# message_params = {:from    => 'fun4saurabh@gmail.com',
   	#                   :to      => @user.email,
   	#                   :subject => 'Sample Mail using Mailgun API',
   	#                   :text    => 'We are using mailgun-ruby for this to s'}
 
-  	# # Send your message through the client
   	# mg_client.send_message ENV['domain'], message_params
+
+    # End 
   end
 end
